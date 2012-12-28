507d19ec9e97b4d72051327c7d2ac1081e59ab45
root <root@Pavel.(none)>
2012-12-28 18:03:14 +0200

    releasedr

--- src/MyProject/MainBundle/Controller/DefaultController.php
+++ src/MyProject/MainBundle/Controller/DefaultController.php
@@ -8,23 +8,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($slug = null)
+    public function indexAction()
+    {           
+        $title='Главная';
+        return $this->render('MyProjectMainBundle::main.html.twig',array('title' => $title,'slug' => ''));
+    }
+    
+     public function categoryAction($slug = null)
     {           
         $title='MainPage';
         $body='MainPage';
-        
         $products = null;
 
         if($slug){
             $category = $this->getDoctrine()->getRepository('MyProjectMainBundle:Category')->findOneBySlug($slug);
             $products = $this->getDoctrine()->getRepository('MyProjectMainBundle:Products')->findByCategory($category->getId());
         }
-        
-        return $this->render('MyProjectMainBundle::main.html.twig',array('title' => $title,'body' => $body, 'products' => $products, 'slug' => $slug));
+
+
+        return $this->render('MyProjectMainBundle:Default:category.html.twig',array('title' => $title,'body' => $body, 'products' => $products, 'slug' => $slug,'category' => $category->getName()));
     }
     
     public function contactsAction()
-    {     
-        return $this->render('MyProjectMainBundle:Default:contacts.html.twig');
+    {   
+        return $this->render('MyProjectMainBundle:Default:contacts.html.twig',array('slug' => ''));
     }
 }
